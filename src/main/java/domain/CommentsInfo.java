3853fc20e21abf10f06bcf964f899d268f816976
Nikoly <turnik3580@gmail.com>
2020-09-18 15:42:51 +0300

    V1

---
+++ src/main/java/domain/CommentsInfo.java
@@ -0,0 +1,16 @@
+package domain;
+
+public class CommentsInfo {
+    private int userId;
+    private String text;
+    private String imageUrl;
+    private String audioUrl;
+    private String videoUrl;
+    private String docUrl;
+    private String EmojiUrl;
+    private int date;
+    private int time;
+    private int numberOfReplies;
+    private int numberOfLikes;
+
+}
